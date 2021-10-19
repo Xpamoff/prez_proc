93a7bcd898cc4400a365163e5bb9315e042d155b
Xpamoff <2005cramar.05@gmail.com>
2021-10-19 19:39:29 +0400

    Second try

--- js/main.js
+++ js/main.js
@@ -96,14 +96,42 @@ $(".compile").on("click", ()=>{
         }
         else return false;
     }
-
+    function time_definer(time){
+        if(time<=60){
+            return 1;
+        }
+        else if(time>60&&time<=120){
+            return 2;
+        }
+        else if(time>120&&time<=180){
+            return 3;
+        }
+        else if(time==180){
+            return 4;
+        }
+        else if(time>180&&time<=360){
+            return 5;
+        }
+        else{
+            return false;
+        }
+    }
 
     let pv = $("#pv").val();
     let kv = $("#kv").val();
-    let time = $("input[name='time']:checked").val();
+    let time = $('#time').val();
+    let sum = $('#sum').val();
 
+    if(time<1||time>360){
+        $('.result').html("Ошибка: Срок кредита не может быть меньше 1 мес. или больше 360 мес.");
+        $('#time').css({'border' : '2px solid red', 'background-color' : 'rgb(255 200 200)'});
+        return 0;
+    }
+    else{
+        $('#time').css({'border' : '2px solid rgba(0, 106, 28, 0.27)', 'background-color' : 'white'});
+    }
     if(pv<15||pv>100){
-        $('.result').html("Результат: ПВ не может быть меньше 15% или больше 100%");
+        $('.result').html("Ошибка: ПВ не может быть меньше 15% или больше 100%");
         $('#pv').css({'border' : '2px solid red', 'background-color' : 'rgb(255 200 200)'});
         return 0;
     }
@@ -111,7 +139,7 @@ $(".compile").on("click", ()=>{
         $('#pv').css({'border' : '2px solid rgba(0, 106, 28, 0.27)', 'background-color' : 'white'});
     }
     if(kv!=4&&kv!=4.5&&kv!=5&&kv!=7&&kv!=10){
-        $('.result').html("Результат: КВ может принимать только значения: 4, 4.5, 5, 7, 10");
+        $('.result').html("Ошибка: КВ может принимать только значения: 4, 4.5, 5, 7, 10");
         $('#kv').css({'border' : '2px solid red', 'background-color' : 'rgb(255 200 200)'});
         return 0;
     }
@@ -119,10 +147,18 @@ $(".compile").on("click", ()=>{
         $('#kv').css({'border' : '2px solid rgba(0, 106, 28, 0.27)', 'background-color' : 'white'});
     }
 
+    let time_of_credit = time;
+
+    time = time_definer(time);
     pv = pv_definer(pv, time);
     kv = kv_definer(kv, time);
 
+
     let result = pv-kv;
-    
-    $('.result').html("Результат: " + result.toFixed(2) + "%");
+    let mes = result/12/100;
+    console.log(mes);
+    let credit = sum*(mes/(1-(1+mes)**(-1*time_of_credit)));
+
+
+    $('.result').html("Ставка: " + result.toFixed(2) + "% <br> Платёж: " + credit.toFixed(4) + " руб.");
 })
